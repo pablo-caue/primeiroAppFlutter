72c00e6555f1c22174091e479bfd2acda9a546b3
pablo-caue <pablocrs2004@gmail.com>
2023-11-11 19:09:45 -0300

    feat: challenge concluded

--- lib/main.dart
+++ lib/main.dart
@@ -81,14 +81,14 @@ class _MyHomePageState extends State<MyHomePage> {
         // TRY THIS: Try changing the color here to a specific color (to
         // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
         // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        backgroundColor: Colors.black,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text("Hello world", style: TextStyle(color: Colors.white)),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+        // in the middle of the parent
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
@@ -104,21 +104,30 @@ class _MyHomePageState extends State<MyHomePage> {
           // action in the IDE, or press "p" in the console), to see the
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
+
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            Visibility(
+              visible: _counter != 0,
+              child: Text(
+                'Você clicou no botão $_counter vez(es)',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
+            Visibility(
+              visible: _counter == 0,
+              child: Text(
+                'Clique no botão',
+                style: Theme.of(context).textTheme.headlineMedium,
+              ),
+            )
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        child: Icon(Icons.add, color: Colors.white),
+        backgroundColor: Colors.black,
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
